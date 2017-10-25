167466dadea6765163bb4d9c7b1152deb91a6e8a
Nathalie Ruano <nathalie.ruano001@gmail.com>
2017-10-25 11:01:26 -0400

    Cleaned and updated function names
    Cleaned up some debugging print statements and changed function names to be meaningful for future users.

--- MothVRstuff/Keyboard.cpp
+++ MothVRstuff/Keyboard.cpp
@@ -32,7 +32,6 @@ static unsigned int fps_frames = 0;
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clears colors
 	glColor3f(255, 255, 255); // color white for our rectangle
-	//time_t start, end;
 	
 	glBegin(GL_QUADS);  //Rectangle drawing
 						// Will be using xp and yp as our changing x-position and y-position in our window
@@ -47,36 +46,22 @@ void display() {
 	glutSwapBuffers(); //done with current frame. Swap to being on the next.
 }
 
-void billybob(int bob) {
+void speedManager(int speed) {
 	
-	//printf("in billybob\n");
-	fps_frames++;
-	//std::cout << "\n" << glutGet(GLUT_ELAPSED_TIME);
+	
 	int delta_t = glutGet(GLUT_ELAPSED_TIME) - fps_start;
 	if (delta_t > 1000) {
 		std::cout << double(delta_t) / double(fps_frames) << std::endl;
 		fps_frames = 0;
 		fps_start = glutGet(GLUT_ELAPSED_TIME);
 	}
-	//while (1) {
+	
 	it1++;
-	//printf("%d\n", it1);
-	//if (it1 % mod == 0) {
+	
 	if (it1 == mod) {
 		it1 = 0;
 		it++;
-		//if (it % 700 == 0) {
-			//time_t end;
-			//end = time(0);
-			//printf("%d", end);
-			//printf("Display func took this amount of time : %0.6d \n", difftime(start, end));
-		//it = 0;
-
-		//}
-		//else if (it % 350 == 0) {
-
-		//}
-		//if (it % (700 / boost) < (350 / boost)) {
+		
 		if (it >= (700 / boost)) {
 			it = 0;
 		}
@@ -86,43 +71,36 @@ void billybob(int bob) {
 		else {
 			xp++;
 		}
-		//printf("We are stuck here forevermore\n");
-		//printf("\n");
+		
 		glutPostRedisplay(); //redraws window
-		glutTimerFunc(delay, billybob, 0);
+		glutTimerFunc(delay, speedManager, 0);
 	}
 	else {
-		//it++;
-		//printf("We are stuck here forevermore again\n");
+		
 		glutPostRedisplay();
-		glutTimerFunc(delay, billybob, 0);
+		glutTimerFunc(delay, speedManager, 0);
 	}
 
 }
 
 void key_pressed(int key, int x, int y) {
-	//xp++;
+
 	switch (key) {
 	case GLUT_KEY_RIGHT:
-		//xp--;
-		//it++;
+
 		if (delayIt > 0) {
 			delayIt--;
 			delay = delayArr[delayIt];
 		}
-		//else {
-		//	xp++;
-		//}
 		glutPostRedisplay(); //redraws window
 		break;
 	case GLUT_KEY_LEFT:
-		//xp++;
-		//it++;
+		
 		if (delayIt < 3) {
 			delayIt++;
 			delay = delayArr[delayIt];
 		}
-		//mod++;
+	
 		glutPostRedisplay(); //redraws window
 		break;
 	}
@@ -185,18 +163,17 @@ int main(int argc, char** argv) {
 
 
 	fps_start = glutGet(GLUT_ELAPSED_TIME);
-	//glutTimerFunc(1000, billybob, 0);
+	//glutTimerFunc(1000, speedManager, 0);
 	glutDisplayFunc(display);
 	//printf("finished display\n");
-	glutTimerFunc(0, billybob, 0);
-	//printf("finished billybob\n");
+	glutTimerFunc(0, speedManager, 0);
+	//printf("finished speedManager\n");
 
 	glClearColor(0, 0, 0, 0);
 	gluOrtho2D(0.0, 800, 0.0, 800);
 	glutSpecialFunc(key_pressed);
 	glutKeyboardFunc(letter_pressed);
 
-	//printf("Where it's hitting");
 	printf("\n");
 	glutMainLoop();
 	//glutTimerFunc(1, billybob, 0);
