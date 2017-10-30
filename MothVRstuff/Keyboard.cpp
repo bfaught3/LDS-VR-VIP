8422695e953c922f0149147cd0990987f00b3cd1
bfaught3 <bfaught3@gatech.edu>
2017-10-30 15:51:34 -0400

    Left and right arrow keys now move camera along x-axis.

--- MothVRstuff/Keyboard.cpp
+++ MothVRstuff/Keyboard.cpp
@@ -1,18 +1,15 @@
-#include <iostream>
-//#include "..\..\..\Libs and Include\Include\glut.h"
-
 #define FREEGLUT_STATIC
 #define _LIB
 #define FREEGLUT_LIB_PRAGMAS 0
-//#include <GL/freeglut_ext.h>
-#include <GL/freeglut_std.h>
+
+// OpenGL is pretty senstive so our header file glut.h has to be the first on our include files
+#include <GL/glut.h> // MUST BE FIRST (not including MACROS. Those are fine first.)
 #include <GL/GL.h>
 #include <GL/GLU.h>
 #include <GL/freeglut.h>
-#include <GL/glut.h>
 #include <time.h>       /* time_t, clock, CLOCKS_PER_SEC */
 #include <math.h>       /* sqrt */
-//#include <glad/glad.h>
+#include <iostream>
 #include <GLFW/glfw3.h>
 #define FREEGLUT_STATIC
 #define _LIB
@@ -28,55 +25,75 @@ int boost = 7;
 bool clear = 0;
 static unsigned int fps_start = 0;
 static unsigned int fps_frames = 0;
+static int num = 550, num2 = 600, num3 = 800;
+float xAccelArr[5] = { -0.5, -0.1, 0.0, 0.1, 0.5 };
+int xAccelIt = 3;
+float xAccel = xAccelArr[xAccelIt];
+
+// angle of rotation for the camera direction
+float angle = 0.0;
+// actual vector representing the camera's direction
+float lx = 0.0f, lz = -1.0f;
+// XZ position of the camera
+float x = 0.0f, z = 0.0f;
 
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clears colors
+	//glLoadIdentity();
+
+	//gluLookAt(400.0f, 400.0f, 100.0f,
+	//	400.0f, 400.0f, 0.0f,
+	//	400.0f, 401.0f, 0.0f);
+
+	//glMatrixMode(GL_PROJECTION);
+	//glLoadIdentity();
+	//gluPerspective(60.0, width / (float)height, 0.1, 100.0);
+	//gluPerspective(60.0, 1.0, 0.1, 100.0);
+	//glMatrixMode(GL_MODELVIEW);
+
+	//gluLookAt(0.0f, 0.0f, 0.0f,
+	//	0.0f, 0.0f, -1.0f,
+	//	0.0f, 1.0f, 0.0f);
+
+	//glViewport(0, 0, 800, 800);
+
 	glColor3f(255, 255, 255); // color white for our rectangle
-	//time_t start, end;
 	
+	gluLookAt(x + lx, 0.0f, z,
+		x + lx, 0.0f, z + lz,
+		0.0f, 1.0f, 0.0f);
+
 	glBegin(GL_QUADS);  //Rectangle drawing
 						// Will be using xp and yp as our changing x-position and y-position in our window
-	glVertex2i(600 + boost * xp, 0 + yp);
-	glVertex2i(550 + boost * xp, 0 + yp);
-	glVertex2i(550 + boost * xp, 800 + yp);
-	glVertex2i(600 + boost * xp, 800 + yp);
-
-
+	glVertex3f(600 + boost * xp, 0 + yp, 0);
+	glVertex3f(550 + boost * xp, 0 + yp, 0);
+	glVertex3f(550 + boost * xp, 800 + yp, 0);
+	glVertex3f(600 + boost * xp, 800 + yp, 0);
 
 	glEnd();
 	glutSwapBuffers(); //done with current frame. Swap to being on the next.
 }
 
-void billybob(int bob) {
+void speedManager(int speed) {
 	
-	//printf("in billybob\n");
 	fps_frames++;
-	//std::cout << "\n" << glutGet(GLUT_ELAPSED_TIME);
 	int delta_t = glutGet(GLUT_ELAPSED_TIME) - fps_start;
 	if (delta_t > 1000) {
 		std::cout << double(delta_t) / double(fps_frames) << std::endl;
 		fps_frames = 0;
 		fps_start = glutGet(GLUT_ELAPSED_TIME);
 	}
-	//while (1) {
+
+	//gluLookAt(0.0f, 0.0f, 10.0f,
+	//	0.0f, 0.0f, 0.0f,
+	//	0.0f, 1.0f, 0.0f);
+	
 	it1++;
-	//printf("%d\n", it1);
-	//if (it1 % mod == 0) {
+	
 	if (it1 == mod) {
 		it1 = 0;
 		it++;
-		//if (it % 700 == 0) {
-			//time_t end;
-			//end = time(0);
-			//printf("%d", end);
-			//printf("Display func took this amount of time : %0.6d \n", difftime(start, end));
-		//it = 0;
-
-		//}
-		//else if (it % 350 == 0) {
-
-		//}
-		//if (it % (700 / boost) < (350 / boost)) {
+		
 		if (it >= (700 / boost)) {
 			it = 0;
 		}
@@ -86,43 +103,52 @@ void billybob(int bob) {
 		else {
 			xp++;
 		}
-		//printf("We are stuck here forevermore\n");
-		//printf("\n");
+		
 		glutPostRedisplay(); //redraws window
-		glutTimerFunc(delay, billybob, 0);
+		glutTimerFunc(delay, speedManager, 0);
 	}
 	else {
-		//it++;
-		//printf("We are stuck here forevermore again\n");
+		
 		glutPostRedisplay();
-		glutTimerFunc(delay, billybob, 0);
+		glutTimerFunc(delay, speedManager, 0);
 	}
 
 }
 
 void key_pressed(int key, int x, int y) {
-	//xp++;
+	float fraction = 0.1f;
 	switch (key) {
-	case GLUT_KEY_RIGHT:
-		//xp--;
-		//it++;
+	case GLUT_KEY_UP:
+
 		if (delayIt > 0) {
 			delayIt--;
 			delay = delayArr[delayIt];
 		}
-		//else {
-		//	xp++;
-		//}
 		glutPostRedisplay(); //redraws window
 		break;
-	case GLUT_KEY_LEFT:
-		//xp++;
-		//it++;
+	case GLUT_KEY_DOWN:
+		
 		if (delayIt < 3) {
 			delayIt++;
 			delay = delayArr[delayIt];
 		}
-		//mod++;
+	
+		glutPostRedisplay(); //redraws window
+		break;
+	case GLUT_KEY_LEFT:
+		if (xAccelIt > 0) {
+			xAccelIt--;
+			xAccel = xAccelArr[xAccelIt];
+		}
+		lx = xAccel;
+		glutPostRedisplay(); //redraws window
+		break;
+	case GLUT_KEY_RIGHT:
+		if (xAccelIt < 4) {
+			xAccelIt++;
+			xAccel = xAccelArr[xAccelIt];
+		}
+		lx = xAccel;
 		glutPostRedisplay(); //redraws window
 		break;
 	}
@@ -152,6 +178,20 @@ void letter_pressed(unsigned char key, int x, int y) {
 		}
 		glutPostRedisplay();
 		break;
+	case 114:
+		printf("We are entering our switch case\n");
+		glPushMatrix();
+		glTranslatef(200, 300, 0);
+		glRotatef(90, 0, 0, 1);
+		glBegin(GL_QUADS);
+		{
+			glVertex2f(-num / 2, -num2 / 2);
+			glVertex2f(-num2 / 2, -num2 / 2);
+			glVertex2f(num2 / 2, num3 / 2);
+			glVertex2f(num / 2, num3 / 2);
+		}
+		glEnd();
+		glPopMatrix();
 	}
 }
 
@@ -178,27 +218,26 @@ int main(int argc, char** argv) {
 	printf("Press left or right arrows to move our rectangle");
 
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	//glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowSize(800, 800);
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("Testing OpenGL");
 
 
 	fps_start = glutGet(GLUT_ELAPSED_TIME);
-	//glutTimerFunc(1000, billybob, 0);
+	//glutTimerFunc(1000, speedManager, 0);
 	glutDisplayFunc(display);
 	//printf("finished display\n");
-	glutTimerFunc(0, billybob, 0);
-	//printf("finished billybob\n");
+	glutTimerFunc(0, speedManager, 0);
+	//printf("finished speedManager\n");
 
 	glClearColor(0, 0, 0, 0);
 	gluOrtho2D(0.0, 800, 0.0, 800);
 	glutSpecialFunc(key_pressed);
 	glutKeyboardFunc(letter_pressed);
 
-	//printf("Where it's hitting");
-	printf("\n");
+	//printf("\n");
 	glutMainLoop();
-	//glutTimerFunc(1, billybob, 0);
 	return 0;
 }
