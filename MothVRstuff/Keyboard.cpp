e5a98668aff71fb683ae733ce86fcdfdd5f9c466
bfaught3 <bfaught3@gatech.edu>
2017-11-07 13:08:55 -0500

    Oscillation is sinusoidal, and calculates a baseline

--- MothVRstuff/Keyboard.cpp
+++ MothVRstuff/Keyboard.cpp
@@ -11,17 +11,24 @@
 #include <math.h>       /* sqrt */
 #include <iostream>
 #include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <NIDAQmx.h>
 #define FREEGLUT_STATIC
 #define _LIB
 #define FREEGLUT_LIB_PRAGMAS 0
+#define PI 3.14159265
+#define DAQmxErrChk(functionCall) if( DAQmxFailed(error=(functionCall)) ) goto Error; else
 
 float xp = 0, yp = 0;
-int it = 0, it1 = 0; //iterator for iterator
+int it = 0, it1 = 0, it2 = 0; //iterator for iterator
 int mod = 1;
-int delayArr[4] = { 5, 10, 20, 100 };
+//int delayArr[4] = { 5, 10, 20, 100 };
+int delayArr[4] = { 4, 8, 16, 80 };
 int delayIt = 0;
 int delay = delayArr[delayIt];
-int boost = 7;
+//double boost = 6.4;
+float boost = 175.0;
+//int boost = 4;
 bool clear = 0;
 static unsigned int fps_start = 0;
 static unsigned int fps_frames = 0;
@@ -36,6 +43,15 @@ float angle = 0.0;
 float lx = 0.0f, lz = -1.0f;
 // XZ position of the camera
 float x = 0.0f, z = 0.0f;
+
+float bias = 0.0;
+
+int32       error = 0;
+TaskHandle  taskHandle = 0;
+int32       read;
+float64     data[200000];
+float64		currentData[2];
+char        errBuff[2048] = { '\0' };
 
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clears colors
@@ -65,47 +81,140 @@ void display() {
 
 	glBegin(GL_QUADS);  //Rectangle drawing
 						// Will be using xp and yp as our changing x-position and y-position in our window
-	glVertex3f(600 + boost * xp, 0 + yp, 0);
-	glVertex3f(550 + boost * xp, 0 + yp, 0);
-	glVertex3f(550 + boost * xp, 800 + yp, 0);
-	glVertex3f(600 + boost * xp, 800 + yp, 0);
+	//glVertex3f(400 + boost * xp, 0 + yp, 0);
+	//glVertex3f(550 + boost * xp, 0 + yp, 0);
+	//glVertex3f(550 + boost * xp, 800 + yp, 0);
+	//glVertex3f(600 + boost * xp, 800 + yp, 0);
+	//glVertex3f(600 +  xp, 0 + yp, 0);
+	//glVertex3f(550 +  xp, 0 + yp, 0);
+	//glVertex3f(550 +  xp, 800 + yp, 0);
+	//glVertex3f(600 +  xp, 800 + yp, 0);
+	glVertex3f(425 + boost * xp, 0 + yp, 0);
+	glVertex3f(375 + boost * xp, 0 + yp, 0);
+	glVertex3f(375 + boost * xp, 800 + yp, 0);
+	glVertex3f(425 + boost * xp, 800 + yp, 0);
+	//printf("%f\n", lx);
 
 	glEnd();
 	glutSwapBuffers(); //done with current frame. Swap to being on the next.
 }
 
+/*
+** This function allows to change the speed of the bar
+*/
 void speedManager(int speed) {
 	
 	fps_frames++;
 	int delta_t = glutGet(GLUT_ELAPSED_TIME) - fps_start;
 	if (delta_t > 1000) {
-		std::cout << double(delta_t) / double(fps_frames) << std::endl;
+		//std::cout << double(delta_t) / double(fps_frames) << std::endl;
 		fps_frames = 0;
 		fps_start = glutGet(GLUT_ELAPSED_TIME);
 	}
 
+	/*
+	** Add the closed-loop stuff here.
+	
+
+	int32       error = 0;
+	TaskHandle  taskHandle = 0;
+	int32       read;
+	float64     data[200000];
+	char        errBuff[2048] = { '\0' };
+	// DAQmx analog voltage channel and timing parameters
+	DAQmxErrChk(DAQmxCreateTask("", &taskHandle));
+
+	// IMPORTANT
+	//changed Dev1 to Dev5 as the connection established. So verify what Dev is being used to update this code. DEV5 is our force torque.
+	DAQmxErrChk(DAQmxCreateAIVoltageChan(taskHandle, "Dev5/ai0", "", DAQmx_Val_Cfg_Default, -10.0, 10.0, DAQmx_Val_Volts, NULL));
+	DAQmxErrChk(DAQmxCfgSampClkTiming(taskHandle, "", 10000.0, DAQmx_Val_Rising, DAQmx_Val_ContSamps, 200000));
+	// DAQmx Start Code
+	DAQmxErrChk(DAQmxStartTask(taskHandle));
+	// DAQmx Read Code
+	//for (int it = 0; it < 1000; it++) {
+	//printf("%f\n", data[it]);
+	//}
+	//printf("%f\n", data[0]);
+	DAQmxErrChk(DAQmxReadAnalogF64(taskHandle, 200000, -1.0, DAQmx_Val_GroupByChannel, data, 200000, &read, NULL));
+	// Stop and clear task
+	printf("Acquired %d samples\n", (int)read);
+	for (int it = 0; it < 2; it++) {
+		printf("%f\n", data[it]);
+	}
+	//printf("Acquired %d samples\n", (int)read);
+Error:
+	if (DAQmxFailed(error))
+		DAQmxGetExtendedErrorInfo(errBuff, 2048);
+	if (taskHandle != 0) {
+		DAQmxStopTask(taskHandle);
+		DAQmxClearTask(taskHandle);
+	}
+	if (DAQmxFailed(error))
+		printf("DAQmx Error: %s\n", errBuff);
+
+	/*
+	** This is where the closed-loop stuff ends
+	*/
+
 	//gluLookAt(0.0f, 0.0f, 10.0f,
 	//	0.0f, 0.0f, 0.0f,
 	//	0.0f, 1.0f, 0.0f);
-	
 	it1++;
 	
-	if (it1 == mod) {
+	if (it1 == 1) {
 		it1 = 0;
 		it++;
 		
-		if (it >= (700 / boost)) {
+		//if (it > (double(800 / boost))) {
+		if (it >= 125) {
+		//if (it > 800) {
+		//if (it >= (2000 / boost)) {
 			it = 0;
 		}
-		if (it < (350 / boost)) {
+		/*
+		if (it < (double(400 / boost))) {
+		//if (it < 62) {
 			xp--;
 		}
-		else {
+		else if (it < (double(800 / boost))) {
 			xp++;
 		}
+		else if (it = (double(800 / boost))) {
+			//xp++;
+			it = 0;
+		}
+		*/
+		//xp = xp - sinf(((float)it / (1256.0 / boost)) * PI);
+		xp = -sinf(((float)it / 62.5) * PI);
+		//xp = xp - sinf(((float)it / (400.0)) * PI);
+		//if (xp*xp < 0.1)
+			//printf("%f", 550 + boost * xp);
+
+		DAQmxErrChk(DAQmxReadAnalogF64(taskHandle, 2, -1.0, DAQmx_Val_GroupByChannel, currentData, 2, &read, NULL));
+		goto Skip;
+	Error:
+		if (DAQmxFailed(error))
+			DAQmxGetExtendedErrorInfo(errBuff, 2048);
+		if (taskHandle != 0) {
+			//DAQmxStopTask(taskHandle);
+			//DAQmxClearTask(taskHandle);
+		}
+		if (DAQmxFailed(error))
+			printf("DAQmx Error: %s\n", errBuff);
+
+	Skip:
+		if (currentData[1] * currentData[1] < 25.0) {
+			lx = currentData[1] - bias;
+		}
+		else {
+			lx = 0.0;
+		}
 		
+		printf("%f\n", lx);
+
 		glutPostRedisplay(); //redraws window
 		glutTimerFunc(delay, speedManager, 0);
+
 	}
 	else {
 		
@@ -178,6 +287,7 @@ void letter_pressed(unsigned char key, int x, int y) {
 		}
 		glutPostRedisplay();
 		break;
+	//Not implemented yet
 	case 114:
 		printf("We are entering our switch case\n");
 		glPushMatrix();
@@ -195,32 +305,63 @@ void letter_pressed(unsigned char key, int x, int y) {
 	}
 }
 
-void oscillate(int key, int i, int x) {
-	switch (key) {
-	case GLUT_KEY_LEFT:
-		while (key == GLUT_KEY_LEFT) {
-			int it = 0;
-			if (it % 20 < 10) {
-				xp--;
-				glutPostRedisplay();
-			}
-			else {
-				xp++;
-				glutPostRedisplay();
-			}
-			it++;
-		}
-		break;
-	}
-}
-
 int main(int argc, char** argv) {
 	printf("Press left or right arrows to move our rectangle");
+
+	/*
+	** Add the closed-loop stuff here.
+	*/
+
+	// DAQmx analog voltage channel and timing parameters
+	DAQmxErrChk(DAQmxCreateTask("", &taskHandle));
+
+	// IMPORTANT
+	//changed Dev1 to Dev5 as the connection established. So verify what Dev is being used to update this code. DEV5 is our force torque.
+	DAQmxErrChk(DAQmxCreateAIVoltageChan(taskHandle, "Dev5/ai0", "", DAQmx_Val_Cfg_Default, -10.0, 10.0, DAQmx_Val_Volts, NULL));
+	DAQmxErrChk(DAQmxCfgSampClkTiming(taskHandle, "", 10000.0, DAQmx_Val_Rising, DAQmx_Val_ContSamps, 200000));
+	// DAQmx Start Code
+	DAQmxErrChk(DAQmxStartTask(taskHandle));
+	printf("\ngot here");
+	// DAQmx Read Code
+	//for (int it = 0; it < 1000; it++) {
+	//printf("%f\n", data[it]);
+	//}
+	printf("%f\n", data[0]);
+	DAQmxErrChk(DAQmxReadAnalogF64(taskHandle, 200000, -1.0, DAQmx_Val_GroupByChannel, data, 200000, &read, NULL));
+	printf("\ngot passed through analog");
+	printf("%f\n", data[199999]);
+	bias = data[0];
+	// Stop and clear task
+	printf("Acquired %d samples\n", (int)read);
+	//for (int it = 0; it < 200000; it++) {
+	//	printf("%f\n", data[it]);
+	//}
+	//printf("%f\n", data[199999]);
+	//printf("Acquired %d samples\n", (int)read);
+	DAQmxErrChk(DAQmxReadAnalogF64(taskHandle, 2, -1.0, DAQmx_Val_GroupByChannel, &data[199998], 2, &read, NULL));
+	printf("\ngot passed through analog");
+	printf("%f\n", data[199999]);
+	/*
+Error:
+	if (DAQmxFailed(error))
+		DAQmxGetExtendedErrorInfo(errBuff, 2048);
+	if (taskHandle != 0) {
+		DAQmxStopTask(taskHandle);
+		DAQmxClearTask(taskHandle);
+	}
+	if (DAQmxFailed(error))
+		printf("DAQmx Error: %s\n", errBuff);
+	*/
+
+	/*
+	** This is where the closed-loop stuff ends
+	*/
 
 	glutInit(&argc, argv);
 	//glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowSize(800, 800);
+	//glutInitWindowSize(1600, 800);
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("Testing OpenGL");
 
@@ -234,10 +375,24 @@ int main(int argc, char** argv) {
 
 	glClearColor(0, 0, 0, 0);
 	gluOrtho2D(0.0, 800, 0.0, 800);
+	//gluOrtho2D(0.0, 1600, 0.0, 800);
+
 	glutSpecialFunc(key_pressed);
 	glutKeyboardFunc(letter_pressed);
+	printf("\n Will do something when the keys are hit");
 
 	//printf("\n");
+	goto Skip;
+Error:
+	if (DAQmxFailed(error))
+		DAQmxGetExtendedErrorInfo(errBuff, 2048);
+	if (taskHandle != 0) {
+		DAQmxStopTask(taskHandle);
+		DAQmxClearTask(taskHandle);
+	}
+	if (DAQmxFailed(error))
+		printf("DAQmx Error: %s\n", errBuff);
+Skip:
 	glutMainLoop();
 	return 0;
 }
