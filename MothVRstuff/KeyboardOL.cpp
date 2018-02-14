f63136f10bc97425a5111e043c84dcae8553c0e5
bfaught3 <bfaught3@gatech.edu>
2018-02-14 11:13:31 -0500

    Add files via upload

--- MothVRstuff/KeyboardOL.cpp
+++ MothVRstuff/KeyboardOL.cpp
@@ -1,6 +1,39 @@
+/*
 #define FREEGLUT_STATIC
 #define _LIB
 #define FREEGLUT_LIB_PRAGMAS 0
+#define _SECURE_SCL_DEPRECATE 0
+#define _CRT_SECURE_NO_WARNINGS 0
+
+// OpenGL is pretty senstive so our header file glut.h has to be the first on our include files
+#include <GL/glut.h> // MUST BE FIRST (not including MACROS. Those are fine first.)
+#include <GL/GL.h>
+#include <GL/GLU.h>
+#include <GL/freeglut.h>
+#include <time.h>       // time_t, clock, CLOCKS_PER_SEC
+#include <math.h>       // sqrt 
+#include <iostream>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <NIDAQmx.h>
+#define FREEGLUT_STATIC
+#define _LIB
+#define FREEGLUT_LIB_PRAGMAS 0
+#define PI 3.14159265
+#define DAQmxErrChk(functionCall) if( DAQmxFailed(error=(functionCall)) ) goto Error; else
+*/
+
+#define FREEGLUT_STATIC
+#define _LIB
+#define FREEGLUT_LIB_PRAGMAS 0
+#define _SECURE_SCL_DEPRECATE 0
+#define _CRT_SECURE_NO_WARNINGS 0
+#define FREEGLUT_STATIC
+#define _LIB
+#define FREEGLUT_LIB_PRAGMAS 0
+#define PI 3.14159265
+#define DAQmxErrChk(functionCall) if( DAQmxFailed(error=(functionCall)) ) goto Error; else
+#define DAQmx_Val_GroupByChannel 0
 
 // OpenGL is pretty senstive so our header file glut.h has to be the first on our include files
 #include <GL/glut.h> // MUST BE FIRST (not including MACROS. Those are fine first.)
@@ -13,11 +46,9 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 #include <NIDAQmx.h>
-#define FREEGLUT_STATIC
-#define _LIB
-#define FREEGLUT_LIB_PRAGMAS 0
-#define PI 3.14159265
-#define DAQmxErrChk(functionCall) if( DAQmxFailed(error=(functionCall)) ) goto Error; else
+#include <time.h>
+//#include <string.h>
+//#include <GL/glui.h>
 
 float xp = 0, yp = 0;
 int it = 0, it1 = 0, it2 = 0; //iterator for iterator
@@ -36,6 +67,7 @@ static int num = 550, num2 = 600, num3 = 800;
 float xAccelArr[5] = { -0.5, -0.1, 0.0, 0.1, 0.5 };
 int xAccelIt = 3;
 float xAccel = xAccelArr[xAccelIt];
+int increment = 0;
 int barwidthIt = 1;
 //float barwidthArr[3] = { 145.8f / 2, 221.5f / 2, 300.9f / 2 };
 float barwidthArr[3] = { 72, 110, 150 };
@@ -48,6 +80,27 @@ float angle = 0.0;
 float lx = 0.0f, lz = -1.0f;
 // XZ position of the camera
 float x = 0.0f, z = 0.0f;
+float aggrlx = 0.0f;
+
+int32       error = 0;
+TaskHandle  taskHandle = 0;
+int32       read;
+float64     data[1400700];
+float64		currentData[1400700];
+float64		currai0[200100];
+float64		currai1[200100];
+float64		currai2[200100];
+float64		currai3[200100];
+float64		currai4[200100];
+float64		currai5[200100];
+int64		currai6[200100];
+float64		currxp[200100];
+float64		currxpcl[200100];	// closed loop
+int32		queueit = 0;
+float64		bias0, bias1, bias2, bias3, bias4, bias5;
+char        errBuff[2048] = { '\0' };
+bool written = 0;
+
 
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clears colors
@@ -130,6 +183,56 @@ void display() {
 	glutSwapBuffers(); //done with current frame. Swap to being on the next.
 }
 
+void writeToFile() {
+	FILE * fileP = NULL;
+
+	time_t rawtime;
+	struct tm *info;
+	char buffer[80];
+
+	time(&rawtime);
+	info = localtime(&rawtime);
+	strftime(buffer, 80, "%m%d%Y", info);
+
+	char filename[80];
+	sprintf(filename, "./Data/Moth_FT_%s_%03d.txt", buffer, increment);
+
+	fileP = fopen(filename, "w");
+	printf("\n%s", filename);
+
+	if (fileP != NULL) {
+		for (int i = 0; i < 200100; i++) {
+			int helperQueue = queueit + i;
+			if (helperQueue >= 200100) {
+				helperQueue -= 200100;
+			}
+			fprintf(fileP, "%f, %f, %f, %f, %f, %f, %f, %f, %lld\n", currxp[helperQueue], currxpcl[helperQueue], currai0[helperQueue], currai1[helperQueue], currai2[helperQueue], currai3[helperQueue], currai4[helperQueue], currai5[helperQueue], currai6[helperQueue]);
+		}
+		fclose(fileP);
+		increment++;
+	}
+	else {
+		printf("\nOur file cannot be written to");
+	}
+
+}
+
+float64 * matrixMult(float64 ai0, float64 ai1, float64 ai2, float64 ai3, float64 ai4, float64 ai5) {
+
+	float64 Fx = ((-0.000352378) * ai0) + (0.020472451 * ai1) + ((-0.02633045) * ai2) + ((-0.688977299) * ai3) + (0.000378075 * ai4) + (0.710008955 * ai5);
+	float64 Fy = ((-0.019191418) * ai0) + (0.839003543 * ai1) + ((-0.017177775) * ai2) + ((-0.37643613) * ai3) + (0.004482987 * ai4) + ((-0.434163392) * ai5);
+	float64 Fz = (0.830046806 * ai0) + (0.004569748 * ai1) + (0.833562339 * ai2) + (0.021075403 * ai3) + (0.802936538 * ai4) + ((-0.001350335) * ai5);
+	float64 Tx = ((-0.316303442) * ai0) + (5.061378026 * ai1) + (4.614179159 * ai2) + ((-2.150699522) * ai3) + ((-4.341889297) * ai4) + ((-2.630773662) * ai5);
+	float64 Ty = ((-5.320003676) * ai0) + ((-0.156640061) * ai1) + (2.796170871 * ai2) + (4.206523866 * ai3) + (2.780562472 * ai4) + ((-4.252850011) * ai5);
+	float64 Tz = ((-0.056240509) * ai0) + (3.091367987 * ai1) + (0.122101875 * ai2) + (2.941467741 * ai3) + (0.005876647 * ai4) + (3.094672928 * ai5);
+
+	Tx = Tx + 94.5 * Fy;
+	Ty = Ty - 94.5 * Fx;
+
+	float64 transformedData[6] = { Fx, Fy, Fz, Tx, Ty, Tz };
+	return transformedData;
+}
+
 /*
 ** This function allows to change the speed of the bar
 */
@@ -143,53 +246,13 @@ void speedManager(int speed) {
 		fps_start = glutGet(GLUT_ELAPSED_TIME);
 	}
 
-	/*
-	** Add the closed-loop stuff here.
-
-
-	int32       error = 0;
-	TaskHandle  taskHandle = 0;
-	int32       read;
-	float64     data[200000];
-	char        errBuff[2048] = { '\0' };
-	// DAQmx analog voltage channel and timing parameters
-	DAQmxErrChk(DAQmxCreateTask("", &taskHandle));
-
-	// IMPORTANT
-	//changed Dev1 to Dev5 as the connection established. So verify what Dev is being used to update this code. DEV5 is our force torque.
-	DAQmxErrChk(DAQmxCreateAIVoltageChan(taskHandle, "Dev5/ai0", "", DAQmx_Val_Cfg_Default, -10.0, 10.0, DAQmx_Val_Volts, NULL));
-	DAQmxErrChk(DAQmxCfgSampClkTiming(taskHandle, "", 10000.0, DAQmx_Val_Rising, DAQmx_Val_ContSamps, 200000));
-	// DAQmx Start Code
-	DAQmxErrChk(DAQmxStartTask(taskHandle));
-	// DAQmx Read Code
-	//for (int it = 0; it < 1000; it++) {
-	//printf("%f\n", data[it]);
-	//}
-	//printf("%f\n", data[0]);
-	DAQmxErrChk(DAQmxReadAnalogF64(taskHandle, 200000, -1.0, DAQmx_Val_GroupByChannel, data, 200000, &read, NULL));
-	// Stop and clear task
-	printf("Acquired %d samples\n", (int)read);
-	for (int it = 0; it < 2; it++) {
-	printf("%f\n", data[it]);
+	if (currai6[queueit - 1] == 0 && !written) {
+		writeToFile();
+		written = 1;
 	}
-	//printf("Acquired %d samples\n", (int)read);
-	Error:
-	if (DAQmxFailed(error))
-	DAQmxGetExtendedErrorInfo(errBuff, 2048);
-	if (taskHandle != 0) {
-	DAQmxStopTask(taskHandle);
-	DAQmxClearTask(taskHandle);
+	if (currai6[queueit - 1] == 1) {
+		written = 0;
 	}
-	if (DAQmxFailed(error))
-	printf("DAQmx Error: %s\n", errBuff);
-
-	/*
-	** This is where the closed-loop stuff ends
-	*/
-
-	//gluLookAt(0.0f, 0.0f, 10.0f,
-	//	0.0f, 0.0f, 0.0f,
-	//	0.0f, 1.0f, 0.0f);
 	it1++;
 
 	if (it1 == 1) {
@@ -217,6 +280,8 @@ void speedManager(int speed) {
 		*/
 		//xp = xp - sinf(((float)it / (1256.0 / boost)) * PI);
 		//xp = -sinf(((float)it / (delay / 2)) * PI);
+		float64 tempxp = xp;
+
 		xp = -sinf(((float)it / (float)(delay / 2)) * (float)PI) * boost;
 
 		//xp = xp - sinf(((float)it / (400.0)) * PI);
@@ -233,6 +298,35 @@ void speedManager(int speed) {
 		//lx = calcFx();
 
 		//printf("Acquired %d samples\n", (int)read);
+
+		DAQmxErrChk(DAQmxReadAnalogF64(taskHandle, -1, -1.0, DAQmx_Val_GroupByChannel, currentData, 1400700, &read, NULL));
+		goto Skip;
+
+	Error:
+		if (DAQmxFailed(error)) {
+			DAQmxGetExtendedErrorInfo(errBuff, 2048);
+			printf("\nDAQmx Error: %s\n", errBuff);
+		}
+
+	Skip:
+		for (int i = 0; i < read; i++) {
+			if (queueit >= 200100) {
+				queueit = 0;
+			}
+			//printf("%f\n", currentData[i]);
+			float64 * tempData = matrixMult((currentData[i] - bias0), (currentData[i + read] - bias1), (currentData[i + (read * 2)] - bias2), (currentData[i + (read * 3)] - bias3), (currentData[i + (read * 4)] - bias4), (currentData[i + (read * 5)] - bias5));
+			currai0[queueit] = tempData[0];
+			//printf("%f", tempData[0]);
+			currai1[queueit] = tempData[1];
+			currai2[queueit] = tempData[2];
+			currai3[queueit] = tempData[3];
+			currai4[queueit] = tempData[4];
+			currai5[queueit] = tempData[5];
+			currai6[queueit] = (int64)currentData[i + (read * 6)];
+			currxp[queueit] = tempxp;
+			currxpcl[queueit] = tempxp - aggrlx;
+			queueit++;
+		}
 
 		glutPostRedisplay(); //redraws window
 		glutTimerFunc(4, speedManager, 0);
@@ -348,6 +442,51 @@ void letter_pressed(unsigned char key, int x, int y) {
 int main(int argc, char** argv) {
 	printf("Press left or right arrows to move our rectangle");
 
+	/*
+	** Add the closed-loop stuff here.
+	*/
+
+	// DAQmx analog voltage channel and timing parameters
+	DAQmxErrChk(DAQmxCreateTask("", &taskHandle));
+
+	// IMPORTANT
+	//changed Dev1 to Dev5 as the connection established. So verify what Dev is being used to update this code. DEV5 is our force torque.
+	DAQmxErrChk(DAQmxCreateAIVoltageChan(taskHandle, "Dev1/ai0:6", "", DAQmx_Val_Cfg_Default, -10.0, 10.0, DAQmx_Val_Volts, NULL));
+	DAQmxErrChk(DAQmxCfgSampClkTiming(taskHandle, "", 10000.0, DAQmx_Val_Rising, DAQmx_Val_ContSamps, 200100));
+	// DAQmx Start Code
+	DAQmxErrChk(DAQmxStartTask(taskHandle));
+	//printf("\ngot here");
+	// DAQmx Read Code
+	//printf("%f\n", data[0]);
+	DAQmxErrChk(DAQmxReadAnalogF64(taskHandle, 200100, -1.0, DAQmx_Val_GroupByChannel, data, 1400700, &read, NULL));
+	//printf("%f\n", data[0]);
+	//printf("\ngot passed through analog");
+	//printf("%f\n", data[199999]);
+	//bias = data[0];
+	// Stop and clear task
+	printf("Acquired %d samples\n", (int)read);
+	//printf("%f\n", data[199999]);
+	//printf("Acquired %d samples\n", (int)read);
+	printf("\ngot passed through analog");
+	printf("%f\n", data[1199999]);
+	for (int i = 0; i < read; i++) {
+		if (queueit >= 200100) {
+			queueit = 0;
+		}
+		currai0[queueit] = data[i];
+		currai1[queueit] = data[i + read];
+		currai2[queueit] = data[i + (read * 2)];
+		currai3[queueit] = data[i + (read * 3)];
+		currai4[queueit] = data[i + (read * 4)];
+		currai5[queueit] = data[i + (read * 5)];
+		currai6[queueit] = (int64)data[i + (read * 6)]; //our trigger buttom. Must be floored to 0 or 1.
+		queueit++;
+	}
+
+	/*
+	** This is where the closed-loop stuff ends
+	*/
+
 	glutInit(&argc, argv);
 	//glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB);
@@ -371,9 +510,24 @@ int main(int argc, char** argv) {
 
 	glutSpecialFunc(key_pressed);
 	glutKeyboardFunc(letter_pressed);
-	printf("\n Will do something when the keys are hit");
+	//printf("\n Will do something when the keys are hit");
 
 	//printf("\n");
+	//glutMainLoop();
+	//return 0;
+
+	goto Skip;
+Error:
+	if (DAQmxFailed(error))
+		DAQmxGetExtendedErrorInfo(errBuff, 2048);
+	if (taskHandle != 0) {
+		DAQmxStopTask(taskHandle);
+		DAQmxClearTask(taskHandle);
+	}
+	if (DAQmxFailed(error))
+		printf("\nDAQmx Error: %s\n", errBuff);
+	getchar();
+Skip:
 	glutMainLoop();
 	return 0;
 }
